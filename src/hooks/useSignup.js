e651376f59de38b0e7a8e301fd588bc514f3c827
jrmeyers92 <JakeRMeyers92@gmail.com>
2022-07-01 22:57:12 -0500

    fix typos

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,37 +1,35 @@
-import { useState } from "react"
-import { projectAuth } from "../firebase/config"
+import { useState } from 'react'
+import { projectAuth } from '../firebase/config'
 
 export const useSignup = () => {
-    const [error, setError] = useState(null);
-    const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
 
-    const signUp = async (email, password, displayName) => {
-        setError(null);
-        setIsPending(true);
+  const signup = async (email, password, displayName) => {
+    setError(null)
+    setIsPending(true)
+  
+    try {
+      // signup
+      const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+      console.log(res.user)
 
-        try {
-            // signup user
-            const res = await projectAuth.createUserWithEmailAndPassword(email, password);
-            
-            console.log(res.user);
+      if (!res) {
+        throw new Error('Could not complete signup')
+      }
 
-            if (!res) {
-                throw new Error("Could not complete signup")
-            }
+      // add display name to user
+      await res.user.updateProfile({ displayName })
 
-            // add displayName to user
-            // {displayName} == {displayName: displayName}
-            await res.user.updateProfile({displayName});
-            setIsPending(false);
-            setError(null);
-
-        }
-        catch (err) {
-            console.log(err);
-            setError(err.message);
-            setIsPending(false);
-        }
+      setIsPending(false)
+      setError(null)
+    } 
+    catch(err) {
+      console.log(err.message)
+      setError(err.message)
+      setIsPending(false)
     }
+  }
 
-    return {signUp, error, isPending }
+  return { signup, error, isPending }
 }
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,54 +1,55 @@
-import styles from "./Signup.module.css"
-import { useState } from "react"
-import {useSignup} from "../../hooks/useSignup"
+import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
 
-const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [displayname, setDisplayname] = useState("");
-  const [signUp, isPending, error] = useSignup();
+// styles
+import styles from './Signup.module.css'
 
+export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const { signup, isPending, error } = useSignup()
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    signUp(displayname, password, email)
-    
+    e.preventDefault()
+    signup(email, password, displayName)
   }
 
   return (
-     
-    <form className={styles['signup-form']} onSubmit={handleSubmit}>
-      <h2>Signup</h2>
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>sign up</h2>
+      
       <label>
-        <span>Username: </span>
-        <input type="text" 
-        onChange={(e) => setDisplayname(e.target.value)}
-        value={displayname}
+        <span>email:</span>
+        <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
         />
       </label>
 
       <label>
-        <span>Email: </span>
-        <input type="email" 
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
+        <span>password:</span>
+        <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
         />
       </label>
 
       <label>
-        <span>Password: </span>
-        <input type="password" 
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
+        <span>display name:</span>
+        <input 
+          type="text" 
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
         />
       </label>
-      
-      {!isPending && <button className="btn">Login</button>}
-      {isPending && <button className="btn" disabled>loading</button>}
-      {error && <p>{error}</p>}
-      
+
+      { !isPending && <button className="btn">sign up</button> }
+      { isPending && <button className="btn" disabled>loading</button>}
+      { error && <p>{error}</p> }
+
     </form>
   )
 }
-
-export default Signup
